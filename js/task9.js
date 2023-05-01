3af6290b72bd0acbf13f96b785b31c920fe2fad6
Zaur <eskendarov.gmail.com>
2023-05-01 03:06:11 +0300

    task 2 for training object - advanced level'

---
+++ js/task9.js
@@ -0,0 +1,59 @@
+// Задача:
+
+// У вас есть список учеников, которые хотят поиграть в игру:
+
+// const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+// Но команд может быть только 3 по 3 человека. Напишите функцию sortStudentsByGroups, которая принимает в себя массив строк.
+
+// Внутри она сначала сортирует имена по алфавиту. Затем распределяет учеников по 3 человека в 3 группы по алфавитному порядку. Эти группы должны быть массивами. Как итог, функция возвращает новый массив с тремя командами и строкой как 4й элемент.
+
+// Пример:
+
+// sortStudentsByGroups(students)  =>
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi'
+// ]
+// Если убрать одно студента из списка, то результат будет:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: -'
+// ]
+// А если добавить одного, то:
+
+// [
+//   [ 'Andrew', 'Ann', 'Bernard' ],
+//   [ 'Cris', 'Josh', 'Mark' ],
+//   [ 'Peter', 'Sam', 'Sandra' ],
+//   'Оставшиеся студенты: Takesi, Somebody'
+// ]
+// То есть, меняется содержимое строки. Все оставшиеся ученики попадают туда.
+
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+	arr.sort();
+	let ArrEnd = [];
+	const Commands = 3,
+		  Players = Math.trunc(arr.length / Commands);
+	for (let i = 0; i < Commands; i++) {
+		ArrEnd[i] = arr.slice(0, Players);
+		arr.splice(0, Players);
+	}
+	if (arr.length !== 0) {
+		ArrEnd.push(`Оставшиеся студенты: ${arr.join(', ')}`);
+	} else {
+		ArrEnd.push('Оставшиеся студенты: -');
+	}
+	return ArrEnd;
+}
+
+console.log(sortStudentsByGroups(students));
+
